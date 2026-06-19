d4c523c8e026754d925a31472dddcb49299fe109
mashan16 <43699905+mashan16@users.noreply.github.com>
2026-06-20 01:18:28 +0800

    Исправить пути импорта в quartz.layout.ts для корректной работы компонентов

--- .github/quartz/quartz.layout.ts
+++ .github/quartz/quartz.layout.ts
@@ -1,5 +1,5 @@
-import { PageLayout, SharedLayout } from "./cfg"
-import * as Component from "./components"
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
